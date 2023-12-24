be079eeae5cc98f63c176ac691bcbc556ec34299
RCGV1 <Benjaminfaer@gmail.com>
2023-12-24 15:07:23 -0800

    Initial Commit

---
+++ HamSearch/HamSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  HamSearchApp.swift
+//  HamSearch
+//
+//  Created by Benjamin Faershtein on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct HamSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
